d98ef26b5d2798b5184a5436a273453b898dcfba
Rakib Ahmed <rakib987@gmail.com>
2021-08-18 03:22:44 -0400

    Working Logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,3 +22,45 @@ modal.on(`click`,event => {
         modal.fadeOut(1000);
     }
 })
+
+// Modal Variables
+var actualInput = $(`.modalInput`);
+var customButton = $(`.fileInput`);
+
+// On File Upload Button Click
+customButton.on(`click`,event => {
+    actualInput.click();
+})
+
+// User File Handler
+actualInput.change((event,fileList) => {
+    fileList = event.target.files;
+    console.log(fileList);
+    var modalContent = $(`<div class="modalContent"></div>`);
+
+    // Generating Elements for Each File
+    Object.values(fileList).forEach((file,index) => {
+        console.log(file);
+        var fileCard = $(`
+            <div class="fileCard">
+                <div class="fileIcon">${
+                    function iconType(icon) {
+                        icon = file.type;
+                        switch (icon) {
+                            case `video/mp4`: 
+                            return `<i class="fas fa-file-video"></i>`;
+                            break;
+                            case `audio/mp3`:
+                            return `<i class="fas fa-file-audio"></i>`;;
+                            break;
+                        }
+                    }
+                }</div>
+                <div class="fileName">${file.name}</div>
+                <div class="fileType">${file.type}</div>
+            </div>
+        `);
+        modalContent.append(fileCard);
+    })
+    modal.append(modalContent);
+})
