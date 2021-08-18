890fa472fe940d6aa57e883754159957e3031b42
Rakib Ahmed <rakib987@gmail.com>
2021-08-18 00:59:31 -0400

    Modal Fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,24 @@
 // Main Script
+
+// Fading In Main Body
+const body = $(`body`);
+body.attr(`style`,`display: none`);
+body.fadeIn(1000);
+
+// Getting Modal
 var modal = $(`.modal`);
 modal.hide();
 
+// Open Modal Animation
 var openModal = $(`.openModal`);
 openModal.on(`click`,event => {
+    modal.addClass(`active`);
     modal.fadeIn(1000);
+})
+
+// If User Clicks Out Of Modal
+modal.on(`click`,event => {
+    if ($(event.target).hasClass(`active`)) {
+        modal.fadeOut(1000);
+    }
 })
